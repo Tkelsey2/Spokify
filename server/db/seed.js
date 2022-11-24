e3f96b68945ab8b6f23b43924ea76cdd9b641b74
Tyler Kelsey <ty1erk@hotmail.com>
2022-11-24 14:56:50 +0000

    "Fetch request in seed.js completed ready for manipulation"

--- server/db/seed.js
+++ server/db/seed.js
@@ -5,40 +5,21 @@ const db = require('./db')
 async function seed(){
     await db.sync({force: true})
 
+    fetch('https://api.napster.com/v2.2/tracks/top?apikey=Y2Q2MGQ4ODgtNmVhOS00NjE5LWEzY2EtZmI5OGQzZTJkMzE2&limit=5')
+     .then(response=> {
+        return response.json();
+     })
+     .then(data =>{
+        console.log(data)
+     })
+
     await Songs.bulkCreate([
 
-        {
-            title: 'Lithium',
-            artist: 'Nirvana',
-            rating: 4,
-            album: 'Nevermind',
-            img: ''
-        }, 
-        {
-            title: 'Mans not hot',
-            artist: 'Big Shaq',
-            rating: 5,
-            album:'Mans not hot (the remixes)',
-            img:''
-        },
-        {
-            title: 'CBAT',
-            artist: 'Hudson Mohawke',
-            rating: 1,
-            album: 'Satin Panthers'
-        },
-        {
-            title: 'The pretender',
-            artist: 'Foo Fighters',
-            rating: 5,
-            album:'Echoes,Silence,Patience & Grace',
-            img: ''
-        }
-
+   
     ])
 
-
 }
+
 
 
 module.exports = seed
